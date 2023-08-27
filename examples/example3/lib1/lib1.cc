860eaa374c53107788e60bf61d21dc6831d88246
Vivek Mishra <v.mishra@f5.com>
2023-08-27 19:51:24 +0530

    Bazel repo with multiple library

---
+++ examples/example3/lib1/lib1.cc
@@ -0,0 +1,5 @@
+#include "lib1.h"
+
+#include <iostream>
+
+void CLib1::PrintHello() { std::cout << "Hello" << std::endl; }
---
+++ examples/example3/lib2/lib2.cc
@@ -0,0 +1,5 @@
+#include "lib2.h"
+
+#include <iostream>
+
+void CLib2::PrintWorld() { std::cout << "World" << std::endl; }
